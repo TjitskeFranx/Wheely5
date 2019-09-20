e95458830bcac55018737f110b4d35629041057e
Tjitske Franx <tjitskefranx@gmail.com>
2019-09-20 14:51:51 +0200

    Python tutorial finished?

--- random-data.py
+++ random-data.py
@@ -1,12 +1,26 @@
+from random import random
+import time
+
 from dcd.entities.thing import Thing
 from dcd.entities.property import PropertyType
 
 from dotenv import load_dotenv
 import os
+
+
+# Let's create a function that generate random values
+def generate_dum_property_values(the_property):
+    # Define a tuple with the current time, and 3 random values
+    values = (random(), random(), random())
+    # Update the values of the property
+    the_property.update_values(values)
+
+
 # The thing ID and access token
 load_dotenv()
 THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
+
 
 # Instantiate a thing with its credential
 my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
@@ -19,6 +33,11 @@ print(my_thing.to_json())
 # If we have no properties, let's create a random one
 my_property = my_thing.find_or_create_property("My Random Property",
                                                PropertyType.THREE_DIMENSIONS)
+# Finally, we call our function to start generating dum values
+while True:
+    generate_dum_property_values(my_property)
+    # Have a 2-second break
+    time.sleep(2)
 
 # Let's have a look at the property, it should
 # contains the name, a unique id and the dimensions
