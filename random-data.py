b51113d6aec650de4ef00b9dc20074613594e8b7
Tjitske Franx <tjitskefranx@gmail.com>
2019-09-20 14:20:32 +0200

    Python tot step 5

---
+++ random-data.py
@@ -0,0 +1,15 @@
+from dcd.entities.thing import Thing
+from dcd.entities.property import PropertyType
+
+from dotenv import load_dotenv
+import os
+# The thing ID and access token
+load_dotenv()
+THING_ID = os.environ['THING_ID']
+THING_TOKEN = os.environ['THING_TOKEN']
+
+# Instantiate a thing with its credential
+my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
+
+# We can fetch the details of our thing
+my_thing.read()
