e124b7828628f5289f79100d1a1a0c860118d1ac
Tjitske Franx <tjitskefranx@gmail.com>
2019-11-01 14:00:58 +0100

    changes from the pi

--- Arduino/Connectie/Connection_to_DCD_HUB.py
+++ Arduino/Connectie/Connection_to_DCD_HUB.py
@@ -11,13 +11,13 @@ from dotenv import load_dotenv
 import os
 import serial
 
-from dcd.entities.thing import Thing
-from dcd.entities.property import PropertyType
+#from dcd.entities.thing import Thing
+#from dcd.entities.property import PropertyType
 
 # The thing ID and access token
 load_dotenv()
-THING_ID = os.environ['THING_ID']
-THING_TOKEN = os.environ['THING_TOKEN']
+#THING_ID = os.environ['THING_ID']
+#THING_TOKEN = os.environ['THING_TOKEN']
 
 # Instantiate a thing with its credential
 my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
